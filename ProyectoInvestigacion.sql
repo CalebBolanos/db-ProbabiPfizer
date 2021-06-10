ea84c6b6a31a27803a93702d391314939bfeeb5d
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2021-06-10 14:55:54 -0500

    Initial commit

---
+++ ProyectoInvestigacion.sql
@@ -0,0 +1,26 @@
+select * from pfizer;
+
+alter table pfizer 
+change column `Events reported` events_reported int(11);
+
+alter table pfizer 
+change column `Sex Code` sex_code text;
+
+alter table pfizer 
+change column `Age Code` age_code text;
+
+select sum(events_reported) from pfizer;
+
+drop view if exists funcion_distribucion_conjunta_pfizer;
+create view funcion_distribucion_conjunta_pfizer as
+select Symptoms, sex, sex_code, age, age_code, events_reported, (events_reported/328965) as probabilidad from pfizer;
+
+select * from funcion_distribucion_conjunta_pfizer;
+
+select probabilidad from funcion_distribucion_conjunta_pfizer 
+where Symptoms = "fatigue" and sex_code = "F" and age_code = "30-39";
+
+select * from funcion_distribucion_conjunta_pfizer where Symptoms = "Death";
+
+select * from funcion_distribucion_conjunta_pfizer 
+where sex_code = "F" and age_code = "30-39" order by probabilidad desc limit 5;
